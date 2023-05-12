99ef6ddeb8eeab4cc91fd419d4264e94b2d09cce
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-05-12 17:43:28 -0300

    feat: função de busca para incluir mais restaurantes disponíveis na paginação da API

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,20 +3,34 @@ import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import axios from 'axios';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>();
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/v1/pratos/')
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/pratos/')
       .then(response => {
         setRestaurantes(response.data.results);
+        setProximaPagina(response.data.next);
       });
   },[]);
+
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(response => {
+        setRestaurantes([...restaurantes,...response.data.results]);
+        setProximaPagina(response.data.next);
+    });
+  }
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {proximaPagina && <button onClick={() => verMais()}>
+      Ver mais
+    </button>}
   </section>)
 }
 
