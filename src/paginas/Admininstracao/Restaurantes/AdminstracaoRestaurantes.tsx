b7131273c39b4170ee363a5bbfd22af9472ba920
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-05-15 14:25:49 -0300

    feat: listar restaurantes no painel adminstrativo

---
+++ src/paginas/Admininstracao/Restaurantes/AdminstracaoRestaurantes.tsx
@@ -0,0 +1,40 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { useEffect, useState } from "react";
+import IRestaurante from "../../../interfaces/IRestaurante";
+import axios from "axios";
+
+export default function AdminstracaoRestaurantes(){
+  const [restaurantes, setRestaurante] = useState<IRestaurante[]>([]);
+
+  useEffect(() =>{
+    axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+      .then(response => {
+        setRestaurante(response.data);
+      });
+  },[]);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              Nome
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {restaurantes.map((restaurante) => {
+            return (
+              <TableRow key={restaurante.id}>
+                <TableCell>
+                  {restaurante.nome}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
